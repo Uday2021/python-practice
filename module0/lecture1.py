acf7305ef754c2e25305fa2c68a9e16eb46f7644
udaysaraswat <udaysaraswat12@gmail.com>
2026-07-03 16:24:05 +0530

    lecture 1 and 2 completed

---
+++ module0/lecture1.py
@@ -0,0 +1,10 @@
+name = 'Uday'
+yearOfExperience = 5
+skills = ['Python', 'React', 'Nodejs']
+biodata = {
+    "name": "Uday",
+    "currentRole": "Software Engineer",
+    "dreamRole": "AI Engineer"
+}
+
+print (name, yearOfExperience, skills, biodata, "Details")
---
+++ module0/lecture2.py
@@ -0,0 +1,47 @@
+# Task 1
+
+age = 21
+account_balance = 99.9
+name = "varun"
+is_teenager = True
+
+print(type(age))
+print(type(account_balance))
+print(type(name))
+print(type(is_teenager))
+
+# Task 2
+user_name = input("Enter your name")
+user_age = input("Enter your age")
+user_city = input("Enter your city")
+
+print(f"Hello {user_name}! \n You are {user_age} years old and live in {user_city}.")
+
+
+# Task 3
+
+first_number = int(input("enter first number"))
+second_number = int(input("enter second number"))
+
+print(f"Addition is {first_number+second_number}, subtraction is {first_number - second_number}, multiplication is {first_number * second_number} and division is {first_number/second_number}")
+
+
+# Task 4
+
+x = 100
+y = x
+
+print(id(x))
+print(id(y))
+
+# Noted here ids is coming same
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(id(a))
+print(id(b))
+
+# Noted here ids is coming different
+
+
