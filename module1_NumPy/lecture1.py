010e78ee1f05a5c5ce904b845ece2222631eb0e1
udaysaraswat <udaysaraswat12@gmail.com>
2026-07-28 21:32:03 +0530

    Numpy lecture 1 completed

---
+++ module1_NumPy/lecture1.py
@@ -0,0 +1,28 @@
+# Task 2
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40])
+
+print(arr)
+
+# [10, 20, 30, 40]
+
+
+# Task 3
+
+print(type(arr))
+
+# class numpy.ndarray
+
+
+# Mentor Challenge 1
+
+arr = np.array([1, 2, 3])
+
+print(arr * 2)
+
+# [2,4,6]
+
+
+
