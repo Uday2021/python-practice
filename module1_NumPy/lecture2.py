c6c5806454d06d8322278e529a65b5dc32f9c4a6
udaysaraswat <udaysaraswat12@gmail.com>
2026-07-30 18:16:17 +0530

    lecture 2 completed

---
+++ module1_NumPy/lecture2.py
@@ -0,0 +1,52 @@
+# Mentor Challenge 1
+
+import numpy as np
+
+a = np.array([1,2,3,4])
+
+print(a.shape)
+print(a.ndim)
+print(a.size)
+
+# (1,4), 1, 4
+
+
+# Mentor Challenge 2
+
+import numpy as np
+
+a = np.array([
+    [1,2],
+    [3,4],
+    [5,6]
+])
+
+print(a.shape)
+print(a.ndim)
+print(a.size)
+
+# (3,2), 2, 6
+
+# Homework
+
+arr1 = np.array([5,10,15])
+
+print(arr1.shape)
+print(arr1.ndim)
+print(arr1.size)
+print(arr1.dtype)
+
+# (1,3), 1, 3, int64
+
+arr2 = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+print(arr2.shape)
+print(arr2.ndim)
+print(arr2.size)
+print(arr2.dtype)
+
+# (3,3), 3, 9, int64
