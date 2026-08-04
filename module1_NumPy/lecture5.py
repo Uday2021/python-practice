7ba707431517e18a4b93beb6b6bc79de294b85e9
udaysaraswat <udaysaraswat12@gmail.com>
2026-08-04 15:55:20 +0530

    lecture 5 completed

---
+++ module1_NumPy/lecture5.py
@@ -0,0 +1,95 @@
+# Task 1
+
+import numpy as np
+
+a = np.array([5,10,15])
+
+print(a + 2)
+print(a * 3)
+
+# [ 7 12 17]
+# [15 30 45]
+
+# Task 2
+
+a = np.array([
+ [1,2],
+ [3,4]
+])
+
+print(a + 100)
+
+# [[101 102]
+#  [103 104]]
+
+# Task 3
+
+a = np.array([
+ [1,2,3],
+ [4,5,6]
+])
+
+b = np.array([10,20,30])
+
+print(a + b)
+
+# [[11 22 33]
+#  [14 25 36]]
+
+# Mentor Challenge 1
+
+import numpy as np
+
+a = np.array([2,4,6])
+
+print(a + 10)
+
+# [12 14 16]
+
+# Mentor Challenge 2
+
+a = np.array([
+ [1,2],
+ [3,4]
+])
+
+print(a * 10)
+
+# [[10 20]
+#  [30 40]]
+
+# Mentor Mega Challenge
+
+a = np.array([
+ [10,20,30],
+ [40,50,60]
+])
+
+b = np.array([1,2,3])
+
+print(a - b)
+
+# [[ 9 18 27]
+#  [39 48 57]]
+
+# Homework
+
+a = np.array([
+ [10,20],
+ [30,40]
+])
+
+print(a + 5)
+print(a - 5)
+print(a * 2)
+print(a / 10)
+
+# [[15 25]
+#  [35 45]]
+# [[ 5 15]
+#  [25 35]]
+# [[20 40]
+#  [60 80]]
+# [[1. 2.]
+#  [3. 4.]]
+
