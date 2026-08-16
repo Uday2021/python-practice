adfbc01202bb2e420cb4b2583944c2df38979cc8
udaysaraswat <udaysaraswat12@gmail.com>
2026-08-16 14:50:09 +0530

    lecture 1 sciket learn completed

---
+++ module4_scikit_learn/lecture1.py
@@ -0,0 +1,15 @@
+# Mentor Challenge 1
+# A. Regression
+# B. Classification
+# C. Classification
+# D. Regression
+
+# Mentor Challenge 2
+# x: Age, Experience, Education
+# y: Salary
+
+
+# Mega Challenge
+# X: Age, Salary, Previous purchase
+# Y: Bought
+# Problem type: Classification
