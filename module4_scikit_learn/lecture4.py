8c392ed5f22e5feacc9700c1af61ae1f991d0eba
udaysaraswat <udaysaraswat12@gmail.com>
2026-08-21 12:47:40 +0530

    lecture 4 completed

---
+++ module4_scikit_learn/lecture4.py
@@ -0,0 +1,21 @@
+# Task 1
+
+# MAE - 50/3 = 16.66
+
+# Task 2
+
+# MSE - 1100/3 = 366.6
+
+# Task 3
+
+# RMSE - 10
+
+# Mega Challenge
+# Model A:
+# MAE = 5
+# RMSE = 7
+
+# Model B:
+# MAE = 5
+# RMSE = 15
+# isse ye indicator milta h ki model B predicted some less accurately as compare to model A.
